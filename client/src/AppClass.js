28579ee3bd41d26a91056f4d1787f49dd8149652
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-04-30 13:41:19 +0700

    init course

---
+++ client/src/AppClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
